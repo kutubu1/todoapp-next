d1a2d9de5dd40aaf20ebcc21666b11b02424be13
kutubu <zutfoom@gmail.com>
2024-05-21 13:22:39 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Action from "./component/page";
+
+export default function Home() {
+  return ( 
+    <main className="w-full bg-blue-800">
+      <Action/>  
+      
+    </main>
+  );
+}
